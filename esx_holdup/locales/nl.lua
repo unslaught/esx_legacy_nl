0b790d7b7b3107f2d84f3917ba1733a9249e1615
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 13:56:30 +0200

    Add files via upload

---
+++ esx_holdup/locales/nl.lua
@@ -0,0 +1,16 @@
+Locales['nl'] = {
+  ['shop_robbery'] = 'Winkel Overval',
+  ['press_to_rob'] = 'Druk op ~INPUT_CONTEXT~ om de ~o~Overval~s~ ~b~%s~s~',
+  ['robbery_timer'] = 'Winkel Overval: ~r~%s~s~ seconden over',
+  ['recently_robbed'] = 'deze winkel is recentelijk overvallen Wacht ~y~%s~s~ seconden om opnieuw te beroven',
+  ['rob_in_prog'] = '~r~Overval bezig bij ~b~%s~s~',
+  ['started_to_rob'] = 'Overval begonnen bij ~y~%s~s~',
+  ['alarm_triggered'] = 'het alarm is afgegaan',
+  ['robbery_complete'] = '~r~Overval Geslaagd ! je ~o~stal~s~ ~g~€%s~s~',
+  ['robbery_complete_at'] = '~r~Overval Geslaagd bij ~y~%s~s~',
+  ['robbery_cancelled'] = 'De overval is geanuleerd!',
+  ['robbery_cancelled_at'] = '~r~De overval bij ~b~%s~s~ is geanuleerd!',
+  ['min_police'] = 'er moeten ten minste ~b~%s agenten~s~ in de stad zijn om overvallen te plegen.',
+  ['robbery_already'] = '~r~een Overval is al bezig.',
+  ['no_threat'] = 'je bent geen gevaar voor de winkel eigenaar',
+}
