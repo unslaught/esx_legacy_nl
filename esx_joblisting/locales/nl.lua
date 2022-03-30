9da34a0a3dad223f3a5f62ab24a93236531d38e1
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 13:59:22 +0200

    Add files via upload

---
+++ esx_joblisting/locales/nl.lua
@@ -0,0 +1,5 @@
+Locales['nl'] = {
+  ['new_job'] = 'je hebt een nieuwe baan!',
+  ['access_job_center'] = 'druk op ~INPUT_PICKUP~ om het ~b~uitzendbureau~s~te openen.',
+  ['job_center'] = 'Uitzendbureau',
+}
