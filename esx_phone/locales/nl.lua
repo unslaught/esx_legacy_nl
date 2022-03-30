58387ea9b7dca606a304b4bec50d399c0853059d
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 14:30:31 +0200

    Add files via upload

---
+++ esx_phone/locales/nl.lua
@@ -0,0 +1,13 @@
+Locales['nl'] = {
+  ['new_message'] = '~b~je hebt een bericht ontvangen:~s~ %s',
+  ['press_take_call'] = '%s - Druk op ~INPUT_CONTEXT~ om op te nemen',
+  ['taken_call'] = '~y~%s~s~ Opgenomen',
+  ['gps_position'] = 'de lokatie is toegevoegd aan je GPS',
+  ['message_sent'] = 'het bericht is verzonden',
+  ['cannot_add_self'] = 'kan niet aan jezelf toevoegen!',
+  ['number_in_contacts'] = 'dit nummer staat al in je contacten',
+  ['contact_added'] = 'contact toegevoegd!',
+  ['contact_removed'] = 'het contact is verwijderd !',
+  ['number_not_assigned'] = 'het nummer is niet toegewezen!',
+  ['invalid_number'] = 'dat is geen geldig nummer!',
+}
