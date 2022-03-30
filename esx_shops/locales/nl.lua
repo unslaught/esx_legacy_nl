a66d1402c6973b77c071bb22cffbaaa3d6b596a9
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 14:52:13 +0200

    Add files via upload

---
+++ esx_shops/locales/nl.lua
@@ -0,0 +1,12 @@
+Locales['nl'] = {
+  ['shop'] = 'Winkel',
+  ['shops'] = 'Winkels',
+  ['press_menu'] = 'druk op ~INPUT_CONTEXT~ om de ~y~winkel~s~te openen.',
+  ['shop_item'] = '€%s',
+  ['bought'] = 'je kocht zojuist ~y~%sx~s~ ~b~%s~s~ voor ~r~€%s~s~',
+  ['not_enough'] = 'je hebt niet ~r~genoeg~s~ geld je ~y~mist~s~ nog~r~€%s~s~!',
+  ['player_cannot_hold'] = 'je hebt niet genoeg ~y~ruimte~s~ in je inventaris!',
+  ['shop_confirm'] = 'koop %sx %s voor €%s?',
+  ['no'] = 'nee',
+  ['yes'] = 'ja',
+}
