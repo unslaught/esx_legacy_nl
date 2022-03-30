10322183af2d0ba7db5ff395cd569ef781bab801
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 13:23:38 +0200

    Add files via upload

---
+++ esx_clotheshop/locales/nl.lua
@@ -0,0 +1,12 @@
+Locales['nl'] = {
+  ['valid_this_purchase'] = 'kopen?',
+  ['yes'] = 'ja',
+  ['no'] = 'nee',
+  ['not_enough_money'] = 'je hebt niet genoeg geld',
+  ['press_menu'] = 'Druk op ~INPUT_CONTEXT~ om de ~y~Kleding Winkel~s~te openen.',
+  ['clothes'] = 'Kleding Winkel',
+  ['you_paid'] = 'je betaalde ~g~€%s~s~',
+  ['save_in_dressing'] = 'wil je je outfit opslaan in je Huis?',
+  ['name_outfit'] = 'naam van je outfit',
+  ['saved_outfit'] = 'je outfit is opgeslagen!',
+}
