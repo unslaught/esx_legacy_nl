b5313e6fd103fea6d36445401c3a03a42d586f4c
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 15:27:35 +0200

    Add files via upload

---
+++ instance/locales/nl.lua
@@ -0,0 +1,8 @@
+Locales['nl'] = {
+  ['left_instance'] = 'je hebt de instance verlaten',
+  ['invite_expired'] = 'uitnodiging verlopen',
+  ['press_to_enter'] = 'druk op ~INPUT_CONTEXT~ om de instance binnen te gaan',
+  ['entered_instance'] = 'instance betreden',
+  ['entered_into'] = '%s kwam de instance binnen',
+  ['left_out'] = '%s heeft de instance verlaten',
+}
