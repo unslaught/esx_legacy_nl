5c7abfc952e11168f8d19b6db729b2c3efd56cf9
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 13:49:41 +0200

    Add files via upload

---
+++ esx_garage/locales/nl.lua
@@ -0,0 +1,5 @@
+Locales['nl'] = {
+	['veh_released'] = 'het voertuig is~g~vrijgegeven',
+	['veh_stored'] = 'het voertuig is~g~opgeslagen',
+	['veh_health'] = 'je moet het voertuig eerst repareren!.',
+}
