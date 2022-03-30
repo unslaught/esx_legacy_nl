3dcf369ff70c960dc5b983ba3bd1546687bff522
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 14:48:01 +0200

    Add files via upload

---
+++ esx_rpchat/locales/nl.lua
@@ -0,0 +1,11 @@
+Locales['nl'] = {
+  ['ooc_prefix'] = 'OOC | %s',
+  ['twt_help'] = 'verstuur een tweet',
+  ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+  ['me_help'] = 'persoonlijke acties',
+  ['me_prefix'] = 'me | %s',
+  ['do_help'] = 'RP informatie',
+  ['do_prefix'] = 'do | %s',
+  ['generic_argument_name'] = 'bericht',
+  ['generic_argument_help'] = 'het bericht',
+}
