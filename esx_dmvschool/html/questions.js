ce2594f6820fb25dcfd1d9112c8047fd5104e65f
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 13:33:37 +0200

    Add files via upload

---
+++ esx_dmvschool/html/questions.js
@@ -0,0 +1,93 @@
+var tableauQuestion = [
+	{
+		question: "Als u 80 km/u rijdt en u nadert een woonwijk, dan moet u::",
+		propositionA: "Jij versnelt",
+		propositionB: "Je houdt je snelheid, als je geen andere voertuigen passeert",
+		propositionC: "Je vertraagt",
+		propositionD: "Je behoudt je snelheid",
+		reponse: "C"
+	},
+
+	{
+		question: "Als je bij een stoplicht rechtsaf slaat, maar een zebrapad ziet, wat doe je dan?:",
+		propositionA: "Je passeert de voetganger",
+		propositionB: "Je controleert of er geen andere voertuigen in de buurt zijn",
+		propositionC: "Je wacht tot de voetganger is overgestoken",
+		propositionD: "Je schiet de voetganger neer en rijdt verder",
+		reponse: "C"
+	},
+
+	{
+		question: "de snelheid in een woonwijk is: __ km/h",
+		propositionA: "30 km/h",
+		propositionB: "50 km/h",
+		propositionC: "40 km/h",
+		propositionD: "60 km/h",
+		reponse: "B"
+	},
+
+	{
+		question: "Voor elke rijstrookwissel moet u::",
+		propositionA: "Check je spiegels",
+		propositionB: "Controleer je dode hoeken",
+		propositionC: "Signaleer wat je doet",
+		propositionD: "al het bovenstaande",
+		reponse: "D"
+	},
+
+	{
+		question: "Welk alcoholpromillage wordt gezien als rijden onder invloed?",
+		propositionA: "0.05%",
+		propositionB: "0.18%",
+		propositionC: "0.08%",
+		propositionD: "0.06%",
+		reponse: "C"
+	},
+
+	{
+		question: "Wanneer mag u bij een stoplicht doorrijden?",
+		propositionA: "Als het groen is",
+		propositionB: "Als er niemand op de kruising is",
+		propositionC: "U bevindt zich in een schoolzone",
+		propositionD: "Als het groen en/of rood is en je slaat rechtsaf",
+		reponse: "D"
+	},
+
+	{
+		question: "Een voetganger heeft een niet oversteken bord, wat doe je?",
+		propositionA: "Je laat ze passeren",
+		propositionB: "Je observeert voordat je verder gaat",
+		propositionC: "Je zwaait om te zeggen dat ze mogen oversteken",
+		propositionD: "Je gaat verder want je stoplicht staat op groen",
+		reponse: "D"
+	},
+
+	{
+		question: "Wat is toegestaan ​​bij het passeren van een ander voertuig?",
+		propositionA: "Je volgt het nauw om het sneller te passeren",
+		propositionB: "Je passeert het zonder de rijbaan te verlaten",
+		propositionC: "U rijdt aan de andere kant van de weg om te passeren",
+		propositionD: "Je overschrijdt de snelheidslimiet om ze te passeren",
+		reponse: "C"
+	},
+
+	{
+		question: "U rijdt op een snelweg die een maximumsnelheid van 120 km/u aangeeft. Maar de meeste verkeersdeelnemers rijden 125 km/u, dus u moet niet harder rijden dan:",
+		propositionA: "120 km/h",
+		propositionB: "125 km/h",
+		propositionC: "130 km/h",
+		propositionD: "110 km/h",
+		reponse: "A"
+	},
+
+	{
+		question: "Wanneer u wordt ingehaald door een ander voertuig, is het belangrijk dat u NIET:",
+		propositionA: "zachter gaat rijden",
+		propositionB: "controleer je spiegels",
+		propositionC: "kijkt naar andere autos",
+		propositionD: "sneller gaat rijden",
+		reponse: "D"
+	
+	},
+]
+
---
+++ esx_dmvschool/localization/nl_esx_dmvschool.sql
@@ -0,0 +1,8 @@
+
+
+INSERT INTO `licenses` (`type`, `label`) VALUES
+	('dmv', 'Theorie Cerficaat'),
+	('drive', 'Rijbewijs'),
+	('drive_bike', 'Motor Rijbewijs'),
+	('drive_truck', 'Vrachtwagen Rijbewijs')
+;
