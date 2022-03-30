aa5321d3096e9bff3370b769ceb79257ee5a8a58
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 18:01:33 +0200

    Add files via upload

---
+++ esx_multicharacter/html/locales/nl.js
@@ -0,0 +1,8 @@
+const translate = new Object();
+
+translate.name = "Naam";
+translate.job = "Werk";
+translate.bank = "Bank";
+translate.money = "Geld";
+translate.gender = "Geslacht";
+translate.dob = "Geboorte Datum";
