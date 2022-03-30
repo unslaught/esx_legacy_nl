05ce6ad6491aa122ba5856f3235395a64476c62f
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 12:12:59 +0200

    Add files via upload

---
+++ esx_billing/locales/nl.lua
@@ -0,0 +1,12 @@
+Locales['nl'] = {
+  ['invoices'] = 'fakturen',
+  ['invoices_item'] = '€%s',
+  ['received_invoice'] = 'je hebt een faktuur ~r~ontvangen~s~',
+  ['paid_invoice'] = 'je ~g~betaalde~s~ een faktuur van~r~€%s~s~',
+  ['no_invoices'] = 'je hebt momenteel geen fakturen om te betalen',
+  ['received_payment'] = 'je~g~ontving~s~ een bedrag van~r~€%s~s~',
+  ['player_not_online'] = 'de speler is niet aanwezig',
+  ['no_money'] = 'je hebt niet genoeg geld voor deze faktuur',
+  ['target_no_money'] = 'de speler~r~heeft niet~s~ genoeg geld om de faktuur te betalen!',
+  ['keymap_showbills'] = 'open het faktuur menu',
+}
