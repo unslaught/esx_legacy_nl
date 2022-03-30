fb7add1264ce0346191514229f1aacb20bcdfda0
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 14:45:35 +0200

    Add files via upload

---
+++ esx_realestateagentjob/locales/nl.lua
@@ -0,0 +1,27 @@
+Locales['nl'] = {
+  ['properties'] = 'Huizen',
+  ['clients'] = 'Klanten',
+  ['realtor'] = 'Makelaar',
+  ['amount_withdraw'] = 'Aantal witwassen',
+  ['invalid_amount'] = 'Ongeldig aantal',
+  ['press_to_access'] = 'Druk op ~INPUT_CONTEXT~ op het menu te openen',
+  ['amount'] = 'Aantal',
+  ['no_play_near'] = 'Geen spelers in de buurt',
+  ['realtors'] = 'Makelaars',
+  ['boss_action'] = 'Baas acties',
+  ['client_poor'] = 'Je klant heeft te weinig geld',
+
+  -- Property menu
+  ['property_name'] = 'Huis',
+  ['property_actions'] = 'Acties',
+  ['property_actionbuttons'] = '{{Assign [Sell]|sell}} {{Assign [Huren]|rent}} {{GPS|gps}}',
+
+  -- Customer menu
+  ['customer_client'] = 'Klant',
+  ['customer_property'] = 'huis',
+  ['customer_agreement'] = 'Contract',
+  ['customer_actions'] = 'Acties',
+  ['customer_contractbuttons'] = '{{Revoke contract|revoke}} {{GPS|gps}}',
+  ['customer_rent'] = 'Verhuurd',
+  ['customer_sell'] = 'Verkocht',
+}
