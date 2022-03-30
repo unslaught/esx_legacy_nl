efb3a2f11800d9d03d0cd456a0791346194defe1
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 13:45:56 +0200

    Add files via upload

---
+++ esx_drugs/locales/nl.lua
@@ -0,0 +1,31 @@
+Locales ['nl'] = {
+  -- weed
+  ['weed_pickupprompt'] = 'Druk op ~INPUT_CONTEXT~ om~g~Cannabis~s~te verzamener.',
+  ['weed_quitprocess'] = 'Druk op ~INPUT_CONTEXT~ om het ~g~Verwerken~s~te stoppen.',
+  ['weed_inventoryfull'] = 'je hebt niet genoeg ruimte in je inventaris voor~g~Cannabis~s~.',
+  ['weed_processprompt'] = 'Druk op ~INPUT_CONTEXT~ om te starten met ~g~Verwerken~s~.',
+  ['weed_processingstarted'] = 'verwerken van ~g~Cannabis~s~ naar ~g~Marijuana~s~...',
+  ['weed_processingfull'] = 'verwerken ~r~geanuleerd~s~ omdat je inventaris vol zit!',
+  ['weed_processingenough'] = '~b~3x~s~ benodigd om ~g~Cannabis~s~ te verwerken.',
+  ['weed_processed'] = 'je verwerkte ~b~3x~s~ ~g~Cannabis~s~ voor ~b~1x~s~ ~g~Marijuana~s~',
+  ['weed_processingtoofar'] = 'verwerking ~r~geanuleerd~s~ omdat je het gebied verliet.',
+
+  -- drug dealer
+  ['dealer_prompt'] = 'Druk op ~INPUT_CONTEXT~ om te praten met de ~r~Drugs Handelaar~s~.',
+  ['dealer_title'] = 'Drugs Handelaar',
+  ['dealer_item'] = '€%s',
+  ['dealer_notenough'] = 'je hebt niet genoeg om te verkopen!',
+  ['dealer_sold'] = 'je verkocht ~b~%sx~s~ ~y~%s~s~ voor ~g~€%s~s~',
+
+  -- license
+  ['license_title'] = 'je hebt hiervoor een vergunning nodig!.',
+  ['license_no'] = 'nee',
+  ['license_bought'] = 'je kocht de ~b~%s~s~ voor ~r~€%s~s~',
+  ['license_bought_fail'] = 'je kan ~b~%s~s~ niet betalen',
+  ['license_weed'] = 'Wiet verwerkings Vergunning',
+
+  -- blips
+  ['blip_weedfield'] = 'Wiet veld',
+  ['blip_weedprocessing'] = 'wiet verwerking',
+  ['blip_drugdealer'] = 'drugs handelaar',
+}
