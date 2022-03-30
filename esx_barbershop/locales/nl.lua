11fab536c6078d3999315a9b67ab0a8f3d907a12
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 12:04:14 +0200

    Add files via upload

---
+++ esx_barbershop/locales/nl.lua
@@ -0,0 +1,9 @@
+Locales['nl'] = {
+  ['valid_purchase'] = 'Kopen?',
+  ['yes'] = 'ja',
+  ['no'] = 'nee',
+  ['not_enough_money'] = 'Je hebt niet genoeg geld',
+  ['press_access'] = 'Druk op ~INPUT_CONTEXT~ om het ~y~Kapper Menu~s~te openen.',
+  ['barber_blip'] = 'Kapper',
+  ['you_paid'] = 'je betaalde ~g~€%s~s~',
+}
