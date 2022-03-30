7f66feb4666a00fcf38401868bc88fd92e3f2893
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-03-30 15:27:21 +0200

    Add files via upload

---
+++ esx_whitelist/locales/nl.lua
@@ -0,0 +1,8 @@
+Locales['nl'] = {
+  ['whitelist_check']     = 'Toeganglijstcheck...',
+  ['not_whitelisted']     = 'Je moet op Toegangslijst staan om op deze server te mogen spelen!',
+  ['whitelist_empty']     = 'Er zijn geen whitelists opgeslagen voor deze server.',
+  ['license_missing']     = 'Fout: Je Identifier ontbreekt!',
+  ['help_whitelist_add']  = 'voeg iemand toe aan de whitelist',
+  ['help_whitelist_load'] = 'herlaad de whitelist',
+}
