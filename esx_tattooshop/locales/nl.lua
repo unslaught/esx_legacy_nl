c8393ef49c6d53207ca3f2cef4e5e059f0d86b41
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-04-02 15:18:59 +0200

    Add files via upload

---
+++ esx_tattooshop/locales/nl.lua
@@ -0,0 +1,11 @@
+Locales['nl'] = {
+  ['tattoo_shop_prompt'] = 'Druk op ~INPUT_PICKUP~ op het menu te ~y~openen~s~.',
+  ['money_amount']       = '<span style="color:green;">€%s</span>',
+  ['part']               = 'part %s',
+  ['go_back_to_menu']    = '< Ga terug',
+  ['tattoo_item']        = 'Tatoeage %s - %s',
+  ['tattoos']            = 'Tatoeages',
+  ['tattoo_shop']        = 'Tatoeage winkel',
+  ['bought_tattoo']      = 'Je hebt deze ~y~tatoeage~s~ gekocht voor ~r~€%s~s~',
+  ['not_enough_money']   = 'Je hebt ~r~niet genoeg geld~s~ voor deze tatoeage. Je komt ~r~€%s~s~ tekort.'
+}
