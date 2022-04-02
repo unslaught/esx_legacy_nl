04b9e8e723d907176447a9915bfae63640778d38
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-04-02 15:18:16 +0200

    Update config.lua

--- esx_tattooshop/config.lua
+++ esx_tattooshop/config.lua
@@ -5,7 +5,7 @@ Config.Size         = {x = 1.5, y = 1.5, z = 1.5}
 Config.Color        = {r = 0, g = 128, b = 255}
 Config.Type         = 1
 
-Config.Locale       = 'en'
+Config.Locale       = 'nl'
 
 Config.Zones = {
 	vector3(1322.6, -1651.9, 51.2),
